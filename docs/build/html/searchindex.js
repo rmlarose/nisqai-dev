f3a9f0af57a0d5ad9d2d7cfddf0c54073fbeead2
Ryan LaRose <rlarose@umich.edu>
2018-11-01 10:17:51 -0400

    Added docs/build/ with html build to see if this will pass on readthedocs.

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["NISQAI"],titleterms:{artifici:0,comput:0,indic:0,intellig:0,librari:0,nisq:0,nisqai:0,open:0,sourc:0,tabl:0}})
