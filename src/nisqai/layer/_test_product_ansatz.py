94d943aa2535012cf92c6303ce9a9aa2e9152e39
Ryan LaRose <rlarose@umich.edu>
2018-12-20 23:59:06 -0500

    Added unit tests for ProductAnsatz class.

---
+++ src/nisqai/layer/_test_product_ansatz.py
@@ -0,0 +1,22 @@
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from _product_ansatz import ProductAnsatz
+
+def test_basic():
+    p = ProductAnsatz(4)
+    assert p.num_qubits == 4
+
+
+if __name__ == "__main__":
+    test_basic()
+    print("All unit tests for ProductAnsatz passed.")
