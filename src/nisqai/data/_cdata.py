b10084a8e2f9dce0ef094452255aad0f3f0fa015
Ryan LaRose <rlarose@umich.edu>
2018-12-24 01:02:30 -0500

    Fleshed out CData (formerly BaseCData) and LabeledCData classes more.

--- src/nisqai/data/_cdata.py
+++ src/nisqai/data/_cdata.py
@@ -10,8 +10,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from numpy import amax
-
+from numpy import amax, average
+from copy import deepcopy
 
 class CData():
     """Classical data class."""
@@ -23,15 +23,31 @@ class CData():
             data [type: numpy array]
                 data values, shape should be (samples, features).
         """
-        self.data = data
+        # TODO: allow data to be a pandas dataframe, 2d list, and
+        # other possible data types people would just want to throw
+        # into the class without thinking about it
+        # for pandas dataframes, just need to convert it to an array
+        self.raw_data = data
+        self.data = deepcopy(self.raw_data)
         self.num_samples, self.num_features = data.shape
+        self._process()
 
-    def normalize(self):
-        """Divides all data by the max element.
+    def _normalize(self):
+        """Divides all data by the max element."""
+        # TODO: complete method
+        #self.data /= amax(self.data)
+        pass
 
-        Modifies self.data in place.
-        """
-        self.data /= amax(self.data)
+    def _center(self):
+        """Subtracts the mean of the data from each entry."""
+        # TODO: complete method
+        #self.data -= average(self.data)
+        pass
+
+    def _process(self):
+        """Processes data."""
+        self._center()
+        self._normalize()
 
     def reduce_features(self, features_to_keep):
         """Performs (classical) principal component analysis
@@ -53,7 +69,29 @@ class CData():
 class LabeledCData(CData):
     """Classical data with labels."""
     def __init__(self, data, labels):
+        """Initialize classical data with labels.
+
+        Args:
+            data [type: numpy array]
+
+        """
         super().__init__(data)
-        self.labels = labels
+        if callable(labels):
+            self.labels = self._compute_labels(labels)
+        else:
+            # TODO: make sure they're an acceptable type,
+            # then convert them to a standard type
+            assert len(labels) == self.num_samples
+            self.labels = labels
 
+    def _compute_labels(self, func):
+        """Returns an array of labels computed according to
+        the input function.
 
+        Args:
+            func [type: callable --> bool]
+                function that returns a zero or one when called
+                on feature vectors
+        """
+        # TODO: complete method
+        return None
