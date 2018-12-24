3cf2089f4899c96891636cb8700c4a8401598993
Ryan LaRose <rlarose@umich.edu>
2018-12-23 22:49:54 -0500

    Added test cases for AlternatingAnsatz class.

--- src/nisqai/layer/_alternating_ansatz_test.py
+++ src/nisqai/layer/_alternating_ansatz_test.py
@@ -11,14 +11,65 @@
 #   limitations under the License.
 
 from nisqai.layer._alternating_ansatz import AlternatingAnsatz
+from pyquil import Program, gates
 
 
 def test_basic():
-    a = AlternatingAnsatz(4)
-    assert a.num_qubits == 4
+    """Makes sure an AlternatingAnsatz can be created and
+    has the correct number of qubits.
+    """
+    a = AlternatingAnsatz(2)
+    assert a.num_qubits == 2
+
+
+def test_match_expected_program():
+    """Tests the output of an AlternatingAnsatz against a given
+    correct output.
+    """
+    a = AlternatingAnsatz(3, structure=[gates.RX])
+
+    # make a program with the correct output
+    p = Program()
+    p00 = p.declare((0, 0), memory_type="REAL")
+    p01 = p.declare((0, 1), memory_type="REAL")
+    p10 = p.declare((1, 0), memory_type="REAL")
+    p11 = p.declare((1, 1), memory_type="REAL")
+    p20 = p.declare((2, 0), memory_type="REAL")
+    p21 = p.declare((2, 1), memory_type="REAL")
+    p.inst(
+        gates.RX(p00, 0),
+        gates.RX(p10, 1),
+        gates.RX(p20, 2),
+        gates.CZ(0, 1),
+        gates.RX(p01, 0),
+        gates.RX(p11, 1),
+        gates.RX(p21, 2),
+        gates.CZ(1, 2)
+    )
+
+    assert a.circuit == p
+
+
+def test_odd_qubits():
+    """Tests the AlternatingAnsatz circuit is correct for an
+    odd number of qubits.
+    """
+    a = AlternatingAnsatz(3)
+    print(a)
+
+
+def test_structure():
+    """Provides a different structure to an AlternatingAnsatz
+    and asserts the output is correct."""
+    a = AlternatingAnsatz(2, [gates.RY])
+    assert a.structure == [gates.RY]
+    print(a)
 
 
 # run the tests
 if __name__ == "__main__":
     test_basic()
+    test_match_expected_program()
+    test_odd_qubits()
+    test_structure()
     print("All tests for AlternatingAnsatz passed.")
