dd805b3491b46fbaa77cf0ac1661f6a04c75e9d0
Ryan LaRose <rlarose@umich.edu>
2019-01-02 07:37:45 -0500

    Edits to one-qubit-classifier notebook to produce plots of decision boundaries.
    Added script to do the entire notebook with a few lines of nisqai.

---
+++ examples/one-qubit-classifier/one-qubit-classifier.py
@@ -0,0 +1,21 @@
+"""Implements the Single Qubit Perceptron using the NISQAI library."""
+
+# =======
+# imports
+# =======
+
+import nisqai
+
+
+# ========
+# get data
+# ========
+
+# returns a CData object with 2 features, five samples, and labels
+data = nisqai.data.random_data(2, 5, [1, 0, 0, 1, 0])
+
+# ===============
+# encode the data
+# ===============
+
+
