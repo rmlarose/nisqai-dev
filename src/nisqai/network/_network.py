5bf9a62208e9c2fabe8b27f7035a6a3afe21021d
Ryan LaRose <rlarose@umich.edu>
2019-02-07 17:39:03 -0500

    Added network files botched in previous commit.

---
+++ src/nisqai/network/_network.py
@@ -0,0 +1,88 @@
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+class Network:
+    """Network class."""
+
+    def __init__(self, layers, computer):
+        """Initializes a network with the input layers.
+
+        Args:
+            layers : iterable
+                Iterable object of network elements.
+
+                Examples:
+                    layers = [product ansatz on one qubit, measurement]
+                    leads to a network of the form
+                        --[Gate]--[Measure]--
+
+                Network elements must be in a valid ordering to create a network.
+
+                Criteria:
+                    (1) Must start with an encoding ansatz.
+                    (2) Must end with a measurement ansatz.
+                    (3) Any number of unitary ansatze can be implemented in between.
+                    (4) If network continues after measurement, an encoding ansatz
+                        must follow a measurement ansatz.
+
+            computer : string
+                Specifies which computer to run the network on.
+
+                Examples:
+                    "Aspen-1-2Q-B"
+        """
+        # TODO: check if ordering of layers is valid
+
+        # TODO: check if computer is valid
+
+        self.layers = layers
+        self.computer = computer
+
+    def _build(self, data_ind):
+        """Builds the network as a sequence of quantum circuits."""
+        # TODO: what about multicircuit networks?
+        # note 2/4/19: I think this could be handled with another class
+
+        # grab the initial encoder circuit for the given index
+        network = self.layers[0][data_ind]
+
+        # add all other layers
+        # TODO: allow self.layers to take sublists
+        # TODO: for example, [encoder, [layer1, layer2, layer3, ...], measure]
+        # TODO: this could make it easier to build networks using, say, list comprehensions
+        for ii in range(1, len(self.layers)):
+            network += self.layers[ii]
+
+        return network
+
+    def propagate(self, parameters, trials):
+        """Runs the network and returns the result.
+
+        Args:
+            parameters : list
+                Any parameters needed for the network.
+
+            trials : int
+                Number of times to run each circuit.
+        """
+        pass
+
+    def __getitem__(self, index):
+        """Returns the network with state preparation for the data
+        point indicated by item.
+
+        Args:
+            index : int
+                Index of data point.
+        """
+        return self._build(index)
---
+++ src/nisqai/network/_network_test.py
@@ -0,0 +1,107 @@
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from numpy import array
+
+from nisqai.layer._base_ansatz import BaseAnsatz
+from nisqai.network._network import Network
+from nisqai.data._cdata import random_data, CData, LabeledCData
+from nisqai.encode._dense_angle_encoding import DenseAngleEncoding
+from nisqai.encode._binary_encoding import BinaryEncoding
+from nisqai.layer._product_ansatz import ProductAnsatz
+from nisqai.measure._measure import Measurement
+from nisqai.encode._encoders import angle_simple_linear
+from nisqai.encode._feature_maps import nearest_neighbor
+
+
+def test_simple():
+    """Tests if a Network can be instantiated."""
+
+    # get components for a simple network
+    cdata = random_data(num_features=2, num_samples=100, labels=[1 if x < 50 else 0 for x in range(100)])
+    encoder = DenseAngleEncoding(cdata, angle_simple_linear, nearest_neighbor(2, 1))
+    layer = ProductAnsatz(1)
+    measure = Measurement(1, range(1))
+    computer = "1q-qvm"
+
+    # make the network
+    qnn = Network([encoder, layer, measure], computer)
+
+    # check some basics
+    assert qnn.computer == computer
+    assert True
+
+
+def test_build_basic():
+    """Tests building a simple Network."""
+    # get the components for a network
+    data = array([[0, 1], [1, 0]])
+    cdata = CData(data)
+    encoder = BinaryEncoding(cdata)
+    layer = ProductAnsatz(2)
+    measure = Measurement(2, [0])
+
+    # make the network
+    qnn = Network([encoder, layer, measure], computer="2q-qvm")
+
+    # build the network
+    net0 = qnn._build(0)
+    net1 = qnn._build(1)
+
+    # checks
+    assert type(net0) == BaseAnsatz
+    assert type(net1) == BaseAnsatz
+
+
+def test_build_multiple_ansatze():
+    """Tests building a Network with multiple sequential unitary ansatze."""
+    # TODO: options for adding ansatze together -- keep parameters the same or define new ones?
+    # get the network components
+    data = array([[0, 1], [1, 0]])
+    cdata = LabeledCData(data, labels=array([1, 0]))
+    encoder = BinaryEncoding(cdata)
+    layer1 = ProductAnsatz(2)
+    layer2 = ProductAnsatz(2)
+    measure = Measurement(2, [0])
+
+    # build the network
+    qnn = Network([encoder, layer1, layer2, measure], "2q-qvm")
+
+    # build the network
+    net0 = qnn._build(0)
+
+    # checks
+    assert type(net0) == BaseAnsatz
+
+
+def test_get_item():
+    """Tests getting the correct circuit."""
+    # get network components
+    data = array([[0], [1]])
+    cdata = LabeledCData(data, labels=array([0, 1]))
+    encoder = BinaryEncoding(cdata)
+    unitary = ProductAnsatz(1)
+
+    # make the network
+    qnn = Network([encoder, unitary, Measurement(1, [0])], "1q-qvm")
+
+    # checks
+    assert type(qnn[0] == BaseAnsatz)
+    print(qnn[0])
+
+
+if __name__ == "__main__":
+    test_simple()
+    test_build_basic()
+    test_build_multiple_ansatze()
+    test_get_item()
+    print("All tests for Network passed.")
