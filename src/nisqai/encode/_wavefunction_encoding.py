a5b955819b6d33ac003a3deb5fc57d8302c87b24
almullay <almulla.yousif38@gmail.com>
2019-02-28 19:31:16 -0800

    Wrote function mapping a complex vector x to a unitary map which sens |0> to x

--- src/nisqai/encode/_wavefunction_encoding.py
+++ src/nisqai/encode/_wavefunction_encoding.py
@@ -10,3 +10,65 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+
+from nisqai.layer._base_ansatz import BaseAnsatz
+from nisqai.data._cdata import CData, LabeledCData
+
+from numpy import array, cos, sin, exp, dot, identity, isclose
+from numpy import identity, delete, linalg, matmul, random # functions yousif used
+from pyquil import Program
+
+from pyquil.quil import DefGate
+
+
+def WavefunctionEncoding(x):
+    """WavefunctioneEncoding class. Encode a vector |x> directly via
+
+    |\psi> = |x> / \ltwonorm(|x>).
+
+
+    With this encoding, an n length vector needs n qubits.
+
+    Args:
+        x : list
+            List of coefficients for the state in canonical binary ordering 00..0, 00..01, 00..10, 00..11, ...
+
+        num_qubits : int
+            The number of qubits in the circuit.
+
+        data : ...
+    """
+    # Use Gram-Schmidt to compute 2^{|x|} - 1 orthogonal rows to x, starting with x and standard basis vectors.
+    # If rows a square matrix form an orthonormal basis, columns will too. Thus we'll have a unitary matrix.
+
+    # Normalize vector
+    x = array(x) / linalg.norm(x)
+
+    standard_basis = identity(len(x))
+
+    # Delete a row so you only have 2^{|x|} - 1 orthogonal vectors
+    # But first, make sure vector input is not in span of one of the vectors used for Gram-Schmidt. (Then starting set won't be a basis.)
+    flag = 0
+    for i in range(len(x)):
+        if abs(1 - abs(dot(x.conj(), standard_basis[i]))) < 1e-2:
+            standard_basis = delete(standard_basis, i, 0)
+            #print(standard_basis)
+            flag = 1
+            break
+    if flag == 0:
+        standard_basis = delete(standard_basis, 0, 0)
+    
+    #print(standard_basis)
+
+    U = []
+    a = x
+    for k in range(len(x)):
+        for i in range(k):
+            #print("here",array(U[i]).conj())
+            a += dot(array(U[i]).conj(),standard_basis[k-1])*array(U[i])
+        if k != 0:
+            a = standard_basis[k-1] - a
+        a = a / linalg.norm(a)
+        U.append(a)
+        a = 0
+    return array(U).T
--- src/nisqai/encode/_wavefunction_encoding_test.py
+++ src/nisqai/encode/_wavefunction_encoding_test.py
@@ -10,3 +10,29 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+from nisqai.encode._wavefunction_encoding import WavefunctionEncoding
+
+from numpy import array, linalg, matmul, random # functions yousif used
+
+def input_output_comparison_test():
+    n = random.randint(4,8)
+    x = random.normal(size=2**n) + 1j*random.normal(size=2**n)
+    x = x / linalg.norm(x)
+    U = WavefunctionEncoding(x)   
+    e_0 = [0]*(2**n)
+    e_0[0] = 1
+    out = matmul(U,array(e_0).T)
+    """
+    print("U",U)
+    print("UU*", matmul(U,U.conj().T))
+    print("U*U", matmul(U.conj().T,U))
+    print("x",x)
+    print("out", out)
+    print(abs(linalg.norm(x - out)))
+    """
+    # Check 2-norm distance between desired state and the state that the created unitary maps |00...0> t0.
+    assert linalg.norm(x - out) < 1e-3
+
+if __name__ == "__main__":
+    input_output_comparison_test()
+    print("All tests for WavefunctionEncoding passed.")
