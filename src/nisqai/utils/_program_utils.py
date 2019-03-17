a4718265f149a1f286c7fb8fb725238102e7ae5e
naezzell <naezzell@gmail.com>
2019-03-16 21:38:08 -0500

    changed ascii_circuit to make_ascii_circuit and added directives support
    In particular, the function now should handle 'PRAGMA', 'MEAUSRE',
    'DEFGATE', and 'DECLARE' properly

--- src/nisqai/utils/_program_utils.py
+++ src/nisqai/utils/_program_utils.py
@@ -20,7 +20,7 @@ def order(program):
     return percolate_declares(program)
 
 
-def ascii_circuit(program, padlen=2, rebind={}):
+def make_ascii_circuit(program, padlen=2, rebind={'MEASURE': 'MSR'}):
     """Creates an ascii circuit from a pyquil program.
 
     Idea is to store a single line (a wire) for each qubit. Then, we the lists of lists
@@ -39,62 +39,89 @@ def ascii_circuit(program, padlen=2, rebind={}):
     TODO: Fix full list of asym for all 2-qubit circuits with asymmetry
     TODO: Fix rebinds to work for gates with parametric dependence (i.e. RX(50)).
     TODO: maybe? Add lines below qubit lines to add connections between two qubit gates?
+    TODO: maybe? Is relying on \t being 4 spaces to ignore DECALRE matrix okay?
     """
+    # keep track of 'directives'
+    directives = ['PRAGMA', 'MEASURE', 'DEFGATE', 'DECLARE']
+
+    # init padding and circuit
     pad = ''.join('-' for n in range(padlen))
     circ = []
+
     # conventions for asymmetric 2-qubit gates (i.e. CNOT) always placed on FIRST
     # argument; otherwise, padded with space
     asym = {'CNOT': "'"}
     for key in rebind.keys():
         if key in asym:
             asym[rebind[key]] = asym[key]
+
     # prepares the ascii string by adding qubits and padding with lines
     qubits = program.get_qubits()
     for qubit in qubits:
         circ.append([str(qubit) + ' '])
 
-    # breaks the program into each gate, in order
+    # breaks the program up by newline
     steps = program.out().split("\n")
 
     # iterates over the steps and creates the "ASCII string"
     for step in steps:
         # redefine step by splitting it up
         step = step.split(' ')
-        # check if 2 qubit gate and make appends to circ
-        if len(step) == 3:
-            gate, qi, qj = step
-            if gate in rebind:
-                gate = rebind[gate]
-            for count, qubit in enumerate(qubits):
-                if int(qi) == qubit:
-                    agate = gate
-                    if gate in asym:
-                        agate += asym[gate]
-                    circ[count][0] += (pad + agate + pad)
+        # handle directives
+        if step[0] in directives:
+            if step[0] == 'PRAGMA':
+                continue
+            elif step[0] == 'DECLARE':
+                continue
+            elif step[0] == 'DEFGATE':
+                continue
+            elif step[0] == 'MEASURE':
+                meas, q, reg = step
+                if meas in rebind:
+                    meas = rebind[meas]
+                for count, qubit in enumerate(qubits):
+                    if int(q) == qubit:
+                        circ[count][0] += (pad + meas + pad)
+                    else:
+                        midpad = ''.join('-' for n in range(len(meas)))
+                        circ[count][0] += (pad + midpad + pad)
+        # handle gates
+        else:
+            # check if 2 qubit gate and make appends to circ
+            if len(step) == 3:
+                gate, qi, qj = step
+                if gate in rebind:
+                    gate = rebind[gate]
+                for count, qubit in enumerate(qubits):
+                    if int(qi) == qubit:
+                        agate = gate
+                        if gate in asym:
+                            agate += asym[gate]
+                        circ[count][0] += (pad + agate + pad)
 
-                elif int(qj) == qubit:
-                    agate = gate
-                    if gate in asym:
-                        agate += ' '
-                    circ[count][0] += (pad + agate + pad)
+                    elif int(qj) == qubit:
+                        agate = gate
+                        if gate in asym:
+                            agate += ' '
+                        circ[count][0] += (pad + agate + pad)
 
-                else:
-                    midpad = ''.join('-' for n in range(len(gate)))
-                    if gate in asym:
-                        midpad += '-'
-                    circ[count][0] += (pad + midpad + pad)
+                    else:
+                        midpad = ''.join('-' for n in range(len(gate)))
+                        if gate in asym:
+                            midpad += '-'
+                        circ[count][0] += (pad + midpad + pad)
 
-        # check if 1 qubit gate and make appends to circ
-        if len(step) == 2:
-            gate, q = step
-            if gate in rebind:
-                gate = rebind[gate]
-            for count, qubit in enumerate(qubits):
-                if int(q) == qubit:
-                    circ[count][0] += (pad + gate + pad)
-                else:
-                    midpad = ''.join('-' for n in range(len(gate)))
-                    circ[count][0] += (pad + midpad + pad)
+            # check if 1 qubit gate and make appends to circ
+            if len(step) == 2:
+                gate, q = step
+                if gate in rebind:
+                    gate = rebind[gate]
+                for count, qubit in enumerate(qubits):
+                    if int(q) == qubit:
+                        circ[count][0] += (pad + gate + pad)
+                    else:
+                        midpad = ''.join('-' for n in range(len(gate)))
+                        circ[count][0] += (pad + midpad + pad)
 
     # do post-processing on circ lists to make a string for printing purposes
     strcirc = ''
--- src/nisqai/utils/_program_utils_test.py
+++ src/nisqai/utils/_program_utils_test.py
@@ -10,8 +10,10 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from nisqai.utils._program_utils import order, ascii_circuit
+from nisqai.utils._program_utils import order, make_ascii_circuit
 from pyquil import Program, gates
+from pyquil.quil import Pragma
+import numpy as np
 
 
 def test_order_basic():
@@ -27,7 +29,7 @@ def test_ascii_circuit_basic():
         [gates.H(0),
          gates.X(0)]
     )
-    drawing = ascii_circuit(prog, 2)
+    drawing = make_ascii_circuit(prog, 2)
 
     assert type(drawing) == str
     print(drawing)
@@ -41,7 +43,7 @@ def test_ascii_circuit_multiple_qubits():
          gates.Z(0),
          gates.H(1)]
     )
-    drawing = ascii_circuit(prog, 2)
+    drawing = make_ascii_circuit(prog, 2)
 
     assert type(drawing) == str
     print(drawing)
@@ -53,7 +55,60 @@ def test_ascii_circuit_two_qubit_gates():
         [gates.H(0),
          gates.CNOT(0, 1)]
     )
-    drawing = ascii_circuit(prog, 2)
+    drawing = make_ascii_circuit(prog, 2)
+
+    assert type(drawing) == str
+    print(drawing)
+
+
+def test_ascii_circuit_with_definted_gate():
+    """Tests drawing a program with defined gate."""
+    prog = Program()
+    Sgate = np.array([[1, 0],
+                      [0, 1]])
+    prog.defgate("S", Sgate)
+    prog += ("S", 0)
+    prog += gates.CNOT(0, 1)
+    drawing = make_ascii_circuit(prog)
+
+    assert type(drawing) == str
+    print(drawing)
+
+
+def test_ascii_circuit_with_pramga1():
+    """Tests drawing a program with a pramga."""
+    prog = Program()
+    prog += Program(Pragma('INITIAL_REWIRING', ['"GREEDY"']))
+    prog += gates.CNOT(0, 1)
+    drawing = make_ascii_circuit(prog)
+
+    assert type(drawing) == str
+    print(drawing)
+
+
+def test_ascii_circuit_with_pramga2():
+    """Tests drawing a program with a different pramga."""
+    prog = Program(
+        [gates.H(0),
+         gates.CNOT(0, 1)]
+    )
+    prog += Pragma('DELAY', [1], str(2))
+    drawing = make_ascii_circuit(prog)
+
+    assert type(drawing) == str
+    print(drawing)
+
+
+def test_ascii_circuit_with_measurement():
+    """Tests drawing a program with a measurement."""
+    prog = Program(
+        [gates.H(0),
+         gates.CNOT(0, 1)]
+    )
+    # adds a measurement to register 'ro'
+    creg = prog.declare("ro", memory_size=1)
+    prog += (gates.MEASURE(0, creg[0]))
+    drawing = make_ascii_circuit(prog)
 
     assert type(drawing) == str
     print(drawing)
@@ -64,4 +119,8 @@ if __name__ == "__main__":
     test_ascii_circuit_basic()
     test_ascii_circuit_multiple_qubits()
     test_ascii_circuit_two_qubit_gates()
+    test_ascii_circuit_with_definted_gate()
+    test_ascii_circuit_with_pramga1()
+    test_ascii_circuit_with_pramga2()
+    test_ascii_circuit_with_measurement()
     print("All unit tests for program_utils passed.")
