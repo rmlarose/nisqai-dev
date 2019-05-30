ee0b5a061b9ffcbd1ea3ebb3de91ef1706953824
Utkarsh <utkarshazad98@gmail.com>
2019-05-30 13:08:01 +0530

    Update __init__.py

--- src/nisqai/utils/__init__.py
+++ src/nisqai/utils/__init__.py
@@ -10,5 +10,5 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from ._program_utils import order, ascii_drawer_simple
-from ._forest_utils import activate_qvm_quilc, deactivate_qvm_quilc
+from nisqai.utils._program_utils import order, ascii_drawer_simple
+from nisqai.utils._forest_utils import activate_qvm_quilc, deactivate_qvm_quilc
